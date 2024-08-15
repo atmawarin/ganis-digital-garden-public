ab3186adeb49d599f45e803ee7f878760fa0fe24
Ganis Angger Atmawarin <ganis@Ganiss-MacBook-Pro.local>
2024-08-15 23:24:46 +0700

    Quartz sync: Aug 15, 2024, 11:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Browntosaurus Digital Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "browntosaurus.site",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
